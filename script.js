8265ba62bbb46a2720a7def14fc30d4f6711b55d
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-03-16 11:30:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-window.promises = [];
+window.promises=[];
+window.promises = Array.from({ length: 5 }, () => new Promise(resolve => {
+  const timeout = Math.floor(Math.random() * 5) + 1;
+  setTimeout(() => resolve(`Promise resolved after ${timeout} seconds`), timeout * 1000);
+}));
 
-// Do not change the code above this
-// add your promises to the array `promises`
-const promises = [];
+Promise.any(window.promises)
+  .then(result => document.getElementById("output").innerHTML = result);
 
-for (let i = 0; i < 5; i++) {
-  const promise = new Promise(resolve => {
-    const timeout = Math.floor(Math.random() * 5 + 1) * 1000;
-    setTimeout(() => {
-      resolve(`Promise ${i+1} resolved in ${timeout/1000} seconds`);
-    }, timeout);
-  });
-  promises.push(promise);
-}
 
-Promise.any(promises)
-  .then(result => {
-    const outputDiv = document.getElementById('output');
-    outputDiv.textContent = result;
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-// Do not change the code above this
-// add your promises to the array `promises`
